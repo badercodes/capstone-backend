85f66d51a4fff2aeda436b76935eb51788712a5d
Bader Alsabah <bader@badercodes.com>
2022-07-01 13:20:55 +0400

    added product schema and model, and tested route with postman

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -3,8 +3,20 @@ const router = express.Router();
 
 // all routes need to go here GET/POST
 
+// this the browse listing route where all proudct listings will be viewable
 router.get("/", function (req, res) {
-  res.send("You have landed on Product route");
+  res.send("You have landed on Products browse page");
+});
+
+// this is where the user goes to list a product
+router.post("/list", function (req, res) {
+  const newEntry = {
+    category: req.body.category,
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+  };
+  res.send(newEntry);
 });
 
 // end of all routes
