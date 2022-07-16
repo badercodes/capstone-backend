24a64acddde21d89d865421ae8bb6ffa69347f43
Bader Alsabah <bader@badercodes.com>
2022-07-16 16:37:30 +0500

    updated User route with error handeling

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,10 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const UserModel = require("../models/UserModel");
 
 // all routes need to go here GET/POST
 
 router.get("/", function (req, res) {
   res.send("You landed on User route");
+});
+
+router.post("/add", function (req, res) {
+  const newUser = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    terms: req.body.terms,
+    picture: req.body.picture,
+  };
+  UserModel.create(newUser)
+    .then((dbReturn) => {
+      console.log(dbReturn);
+      res.send("User data submitted successfully");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("ERROR - User details not submitted");
+    });
 });
 
 // end of all routes
