b9e7487d7e9b6e798fa3a8c0e68a757775c7b063
Bader Alsabah <bader@badercodes.com>
2022-07-23 16:05:32 +0500

    using cors package instead of setting headers manually

--- server.js
+++ server.js
@@ -51,15 +51,15 @@ server.post("/name", function (req, res) {
 
 // Monkey buisness with CORS
 // Add Access Control Allow Origin headers
-server.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500", "*");
-  res.setHeader("Content-Type", "application/json");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500", "*");
+//   res.setHeader("Content-Type", "application/json");
+//   next();
+// });
 
 //~ END of Monkey buisness with CORS ~//
 // More CORS monkey buisness
-// const cors = require("cors");
+const cors = require("cors");
 // const corsOptions = {
 //   origin: "*",
 //   credentials: true, //access-control-allow-credentials:true
@@ -67,7 +67,7 @@ server.use((req, res, next) => {
 // };
 
 // server.use(cors(corsOptions)); // Use this after the variable declaration
-
+server.use(cors());
 // headers.append("Access-Control-Allow-Origin", "http://localhost:3000");
 // headers.append("Access-Control-Allow-Credentials", "true");
 
